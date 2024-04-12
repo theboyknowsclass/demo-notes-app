f73bc33c36d85572c93f55a3ed69837b0e1cf4ca
Gautam Bhatnagar <gautam.bhatnagar@theboyknowsclass.com>
2024-04-12 13:28:37 +0100

    Setting up our React app

---
+++ packages/frontend/src/main.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.tsx";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./index.css";
+import { BrowserRouter as Router } from "react-router-dom";
+import { Amplify } from "aws-amplify";
+import config from "./config.ts";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <Router>
+      <App />
+    </Router>
+  </React.StrictMode>
+);
